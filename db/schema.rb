141b3e8a9a296f6df6d2fdd92f54d3bdb0eca8f1
Aditya27 <adityapore2711@gmail.com>
2016-01-28 13:57:08 -0500

    Finished with alpha blog, updated views, associations, UI

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122154145) do
+ActiveRecord::Schema.define(version: 20160127180859) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
